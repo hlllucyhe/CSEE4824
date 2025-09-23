139a578e1b148e23c732f058a91c53b22fcdcc74
Lucy He <61702333+hlllucyhe@users.noreply.github.com>
2025-09-23 10:04:20 -0400

    first commit

---
+++ hw1.c
@@ -0,0 +1,52 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define REPEAT 10000
+
+inline void clflush(volatile void *p) {
+    asm volatile("clflush (%0)" :: "r"(p));
+}
+
+inline uint64_t rdtsc() {
+    unsigned long a, d;
+    asm volatile("rdtsc" : "=a"(a), "=d"(d));
+    return a | ((uint64_t)d << 32);
+}
+
+inline void memtest(size_t bytes) {
+    uint64_t start, end, clock;
+    char *lineBuffer = (char *)malloc(bytes);
+    char *lineBufferCopy = (char *)malloc(bytes);
+
+    for (int i = 0; i < bytes; i++) {
+        lineBuffer[i] = '1';
+    }
+
+    clock = 0;
+    for (long rep = 0; rep < REPEAT; rep++) {
+        start = rdtsc();
+        memcpy(lineBufferCopy, lineBuffer, bytes);
+        end = rdtsc();
+        for (size_t offset = 0; offset < bytes; offset += 64) {
+            clflush(lineBuffer + offset);
+            clflush(lineBufferCopy + offset);
+        }
+        clock = clock + (end - start);
+        printf("%llu ticks to copy %zuB\n", (end - start), bytes);
+    }
+
+    printf("took %llu ticks total\n", clock);
+}
+
+int main(int ac, char **av) {
+    printf("------------------------------\n");
+    const int exps[] = {6,7,8,9,10,11,12,13,14,15,16,20,21};
+    for (int i = 0; i< 13; i++) {
+        size_t bytes = (size_t) 1 << exps[i];
+        memtest(bytes);
+        printf("------------------------------\n");
+    }
+    return 0;
+}
