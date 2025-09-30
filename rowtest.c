8af6b18ad959832eba65ab84fa1a01a0975df668
Jiaeve <1186359292@qq.com>
2025-09-30 10:19:48 -0400

    Update rowtest.c

--- rowtest.c
+++ rowtest.c
@@ -45,7 +45,7 @@ void rowtest() {
         uint64_t t1 = rdtsc();
         sum_first_same += (t1 - t0);
 
-        // clflush(addr2_same);
+        clflush(addr2_same);
         t0 = rdtsc();
         tmp = *addr2_same;
         t1 = rdtsc();
@@ -60,7 +60,7 @@ void rowtest() {
         t1 = rdtsc();
         sum_first_diff += (t1 - t0);
 
-        //clflush(addr2_diff);
+        clflush(addr2_diff);
         t0 = rdtsc();
         tmp = *addr2_diff;
         t1 = rdtsc();
