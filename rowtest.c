f14e4df010ebaaeeeffc61ac6986883e7ce9254a
Jiaeve <1186359292@qq.com>
2025-09-30 11:10:38 -0400

    Update rowtest.c

--- rowtest.c
+++ rowtest.c
@@ -6,7 +6,7 @@
 
 #define REPEAT 1000000
 #define CACHELINE 64
-#define ROW_SIZE (32*1024)   // row size = 32KB
+#define ROW_SIZE (16*1024)   // row size = 32KB
 
 inline void clflush(volatile void *p) {
     _mm_clflush(p);
