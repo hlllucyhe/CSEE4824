ee803e8c5f822e40bbe3d46cdd0f667db04ccb6c
Lucy He <61702333+hlllucyhe@users.noreply.github.com>
2025-10-01 02:57:27 -0400

    slightly modified comments

--- rowtest.c
+++ rowtest.c
@@ -25,7 +25,7 @@ inline void memory(void *dst, void *src, size_t n) {
 long int rep;
 
 void test_open_vs_closed_row() {
-    // Allocate large buffers to ensure we're working in DRAM
+    // Allocate large buffers using mmap to ensure we are working in DRAM
     char* buffer1 = (char*) mmap(NULL, BUFFER_SIZE, PROT_READ | PROT_WRITE, 
                                  MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
     char* buffer2 = (char*) mmap(NULL, BUFFER_SIZE, PROT_READ | PROT_WRITE, 
@@ -59,8 +59,10 @@ void test_open_vs_closed_row() {
     time_first_access = end - start;
     printf("First access to row 1: %llu ticks\n", time_first_access);
     
+    // Flush both addresses
     clflush(addr1);
     clflush(addr2);
+
     // Access to different row2
     start = rdtsc();
     *addr2 = 'D';
@@ -68,8 +70,10 @@ void test_open_vs_closed_row() {
     time_second_access = end - start;
     printf("First access to row2: %llu ticks\n", time_second_access);
 
+    // Flush both addresses
     clflush(addr1);
     clflush(addr2);
+
     // Access to same row2
     start = rdtsc();
     *addr2 = 'D';
@@ -80,7 +84,7 @@ void test_open_vs_closed_row() {
     // Clean up
     munmap(buffer1, BUFFER_SIZE);
     munmap(buffer2, BUFFER_SIZE);
-    }
+}
     
 
 int main(int ac, char **av) {
