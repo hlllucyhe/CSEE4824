f9c955822ab425592928246b553cd9ae17ef0f91
Jiaeve <1186359292@qq.com>
2025-11-06 17:24:02 -0500

    odify vectorized_multiply.c

--- hw3/vectorized_multiply.c
+++ hw3/vectorized_multiply.c
@@ -2,32 +2,51 @@
 #include <stddef.h>
 #include <immintrin.h>
 
-// vectorized multiplication using intrinsics
-void multiply_vectorized(uint32_t *a, size_t na, uint32_t *b, size_t nb, uint32_t *result) {
-    // Initialize result array to zero
-    memset(result, 0, (na + nb) * sizeof(uint32_t));
+#define MAX_DIGITS 2048  // max digits for each number 1024
+#define BASE 10          // base of each bit 10
 
-    for (size_t i = 0; i < na; i++) {
+int stringToBigInt(const char *str, uint32_t *num) {
+    int len = strlen(str);
+    for (int i = 0; i < len; i++) {
+        num[i] = str[len - 1 - i] - '0';
+    }
+    return len;
+}
+
+void printBigInt(uint64_t *num, int len) {
+    int i = len - 1;
+    while (i > 0 && num[i] == 0) i--; 
+    for (; i >= 0; i--)
+        printf("%llu", (unsigned long long)num[i]);
+    printf("\n");
+}
+
+// vectorized multiplication using intrinsics
+void multiply_vectorized(uint32_t *A, size_t lenA, uint32_t *B, size_t lenB, uint32_t *C) {
+    // Initialize result array to zero
+    memset(C, 0, (lenA + lenB) * sizeof(uint32_t));
+
+    for (size_t i = 0; i < lenA; i++) {
         // broadcast a[i] to all elements of a vector
-        __m256i a_vec = _mm256_set1_epi32(a[i]);
+        __m256i A_vec = _mm256_set1_epi32(A[i]);
         uint64_t carry = 0;
 
-        for (size_t j = 0; j < nb; j += 8) {
+        for (size_t j = 0; j < lenB; j += 8) {
             // load 8 elements from b and result
-            __m256i b_vec = _mm256_loadu_si256((__m256i*)&b[j]);
-            __m256i res_vec = _mm256_loadu_si256((__m256i*)&result[i + j]);
+            __m256i B_vec = _mm256_loadu_si256((__m256i*)&B[j]);
+            __m256i C_vec = _mm256_loadu_si256((__m256i*)&C[i + j]);
 
             // perform multiplication in 8 parallel lanes
-            __m256i prod_low = _mm256_mullo_epi32(a_vec, b_vec);
-            __m256i prod_high = _mm256_mulhi_epu32(a_vec, b_vec);
+            __m256i prod_low = _mm256_mullo_epi32(A_vec, B_vec);
+            __m256i prod_high = _mm256_mulhi_epu32(A_vec, B_vec);
 
             // add carry to the lower part
             __m256i carry_vec = _mm256_set1_epi32((uint32_t)carry);
-            __m256i sum_low = _mm256_add_epi32(res_vec, prod_low);
+            __m256i sum_low = _mm256_add_epi32(C_vec, prod_low);
             __m256i sum_with_carry = _mm256_add_epi32(sum_low, carry_vec);
 
             // Store the lower part of the result
-            _mm256_storeu_si256((__m256i*)&result[i + j], sum_with_carry);
+            _mm256_storeu_si256((__m256i*)&C[i + j], sum_with_carry);
 
             // Calculate new carry
             __m256i overflow_mask = _mm256_cmpgt_epi32(sum_with_carry, sum_low);
@@ -37,6 +56,30 @@ void multiply_vectorized(uint32_t *a, size_t na, uint32_t *b, size_t nb, uint32_
             carry += _mm256_extract_epi32(prod_high, 0);
         }
 
-        result[i + nb] += (uint32_t)carry;
+        C[i + lenB] += (uint32_t)carry;
     }
+}
+int main() {
+    char strA[MAX_DIGITS], strB[MAX_DIGITS];
+    uint32_t A[MAX_DIGITS], B[MAX_DIGITS];
+    uint64_t C[2 * MAX_DIGITS];
+    memset(A, 0, sizeof(A));
+    memset(B, 0, sizeof(B));
+    memset(C, 0, sizeof(C));
+
+    printf("Please enter the first number A:\n");
+    scanf("%s",strA);
+
+    printf("Please enter the first number B:\n");
+    scanf("%s",strB);
+
+    //convert string to arrays
+    int lenA = stringToBigInt(strA, A);
+    int lenB = stringToBigInt(strB, B);
+
+    multiply_vectorized(A, lenA, B, lenB, C);
+
+    printf("Result = ");
+    printBigInt(C, lenA + lenB);
+    return 0;
 }
